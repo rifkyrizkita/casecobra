dbba207d44e6426d15df10ce36ca7f71a358953e
Rifky <rifky.rizkita.g@gmail.com>
2024-08-11 18:58:56 +0700

    2nd commit

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -47,7 +47,7 @@ const DesignConfigurator = ({
   const { toast } = useToast();
   const router = useRouter();
 
-  const { mutate: saveConfig } = useMutation({
+  const { mutate: saveConfig, isPending } = useMutation({
     mutationKey: ["save-config"],
     mutationFn: async (args: SaveConfigArgs) => {
       await Promise.all([saveConfiguration(), _saveConfig(args)]);
@@ -395,6 +395,9 @@ const DesignConfigurator = ({
                 )}
               </p>
               <Button
+              isLoading={isPending}
+                disabled={isPending}
+                loadingText="Saving"
                 onClick={() =>
                   saveConfig({
                     configId,
---
+++ src/components/PhonePreview.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { CaseColor } from "@prisma/client";
+import { useEffect, useRef, useState } from "react";
+import { AspectRatio } from "./ui/aspect-ratio";
+import { cn } from "@/lib/utils";
+
+const PhonePreview = ({
+  croppedImageUrl,
+  color,
+}: {
+  croppedImageUrl: string;
+  color: CaseColor;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [renderedDimension, setRenderedDimension] = useState({
+    height: 0,
+    width: 0,
+  });
+
+  const handleResize = () => {
+    if (!ref.current) return
+    const { width, height } = ref.current.getBoundingClientRect()
+    setRenderedDimension({ width, height })
+
+  }
+
+  useEffect(() => {
+    handleResize()
+    window.addEventListener("resize", handleResize)
+    return () =>  window.removeEventListener("resize", handleResize)
+  }, [ref.current])
+
+  let caseBackgroundColor = "bg-zinc-950"
+  if (color === "blue") {
+    caseBackgroundColor = "bg-blue-950"
+  }
+  if (color === "rose") {
+    caseBackgroundColor = "bg-rose-950"
+  }
+
+  return (
+    <AspectRatio ref={ref} ratio={3000 / 2001} className="relative">
+      <div
+        className="absolute z-20 scale-[1.0352]"
+        style={{
+          left:
+            renderedDimension.width / 2 -
+            renderedDimension.width / (1216 / 121),
+          top: renderedDimension.height / 6.22,
+        }}
+      >
+        <img
+          width={renderedDimension.width / (3000 / 637)}
+          className={cn("phone-skew relative z-20 rounded-b-[10px] rounded-t-[15px] md:rounded-b-[20px] md:rounded-t-[30px]", 
+          caseBackgroundColor)}
+          src={croppedImageUrl}
+        />
+      </div>
+      <div className="relative h-full w-full z-40">
+        <img src="/clearphone.png" alt="phone" 
+        className="pointer-events-none h-full w-full antialiased rounded-md"
+        />
+      </div>
+    </AspectRatio>
+  );
+};
+
+export default PhonePreview;
